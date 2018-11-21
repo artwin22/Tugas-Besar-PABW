a63d9bfc88c185f95a93647dbef4de1f6edd9409
artwin22 <muhamad.14116050@student.itera.ac.id>
2018-11-21 19:55:42 +0700

    Create developer.php

---
+++ application/views/web/developer.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>visitlampung.id - Portal Wisata Provinsi Lampung</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="<?php echo base_url('assets/fontawesome-free/css/all.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('https://fonts.googleapis.com/css?family=Varela+Round') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i') ?>" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url('css/grayscale.min.css') ?>" rel="stylesheet">
+
+    <style>
+      html {
+        box-sizing: border-box;
+      }
+
+      *, *:before, *:after {
+        box-sizing: inherit;
+      }
+
+      .column {
+        width: 30.3%;
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: 16px;
+        padding: 0 8px;
+      }
+
+      @media screen and (max-width: 650px) {
+        .column {
+          width: 90%;
+          display: block;
+        }
+      }
+
+      .card {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+      }
+
+      .container {
+        padding: 0 16px;
+      }
+
+      .container::after, .row::after {
+        content: "";
+        clear: both;
+        display: table;
+      }
+
+      .title {
+        color: grey;
+        text-align: center;
+      }
+
+      .button {
+        border: none;
+        outline: 0;
+        display: inline-block;
+        padding: 8px;
+        color: white;
+        background-color: #303030;
+        text-align: center;
+        cursor: pointer;
+        width: 100%;
+      }
+
+      .button:hover {
+        background-color: #af9d00;
+        color: black;
+      }
+      h2{
+        font-size: 22px;
+        text-align: center;
+      }
+      </style>
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="<?php echo base_url().'index.php/web/overview/' ?>">visitlampung.id</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="<?php echo base_url().'index.php/web/overview/main' ?>">BACK TO HOME</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Developer Team -->
+    <section id="destinasiwisata" class="projects-section bg-dark">
+      <h1 style="text-align:center; color:white;">Developer Team</h1><br><br>
+
+      <!-- FIRST ROW -->
+      <div class="row">
+        <!-- M. Arwin Wijaya -->
+        <div class="column">
+          <div class="card">
+            <img src="<?php echo base_url('img/developer/arwin.jpg') ?>" alt="M. Arwin" style="width:100%">
+            <div class="container">
+              <h2>Muhamad Arwin Wijaya</h2>
+              <p class="title">CEO & Founder</p>
+              <p>Saya tidak mengajarkan tentang hal yang bisa dipelajari oleh mesin, tetapi saya mengajarkan tentang tata krama, attitude dan sopan santun</p>
+              <p>artwin98@gmail.com</p>
+              <p><button class="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+
+        <!-- Ismail Adhan Kusuma -->
+        <div class="column">
+          <div class="card">
+            <img src="<?php echo base_url('img/developer/mail.jpg') ?>" alt="Ismail Adhan" style="width:100%">
+            <div class="container">
+              <h2>Ismail Adhan Kusuma</h2>
+              <p class="title">CO-Founder</p>
+              <p>Lakukanlah</p>
+              <p>ismailadhan21@gmail.com</p>
+              <p><button class="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+
+        <!-- Hanif Abdillah -->
+        <div class="column">
+          <div class="card">
+            <img src="<?php echo base_url('img/developer/hanif.jpg') ?>" alt="Hanif" style="width:100%">
+            <div class="container">
+              <h2>Hanif Abdillah</h2>
+              <p class="title">Creative</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>example@example.com</p>
+              <p><button class="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <br><br>
+      <!-- SECOND ROW -->
+      <div class="row">
+        <!-- Reza Aji Pratama -->
+        <div class="column">
+          <div class="card">
+            <img src="<?php echo base_url('img/developer/jore.jpg') ?>" alt="Reza Aji Pratama" style="width:100%">
+            <div class="container">
+              <h2>Reza Aji Pratama</h2>
+              <p class="title">Back-End</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>rezabachdim21@gmail.com</p>
+              <p><button class="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+
+        <!-- Diah Ayu Wulan -->
+        <div class="column">
+          <div class="card">
+            <img src="<?php echo base_url('img/developer/diah.jpg') ?>" alt="Diah Ayu" style="width:100%">
+            <div class="container">
+              <h2>Diah Ayu Wulan</h2>
+              <p class="title">Marketing</p>
+              <p>.</p>
+              <p>diaahayuwulan@gmail.com</p>
+              <p><button class="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+
+        <!-- Iqbal Sanjaya -->
+        <div class="column">
+          <div class="card">
+            <img src="<?php echo base_url('img/developer/iqbal.jpg') ?>" alt="Iqbal" style="width:100%">
+            <div class="container">
+              <h2>Iqbal Sanjaya</h2>
+              <p class="title">Front-End</p>
+              <p>.</p>
+              <p>iqbalsanjaya909@gmail.com</p>
+              <p><button class="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+    <!-- Contact Section -->
+    <section class="contact-section bg-black">
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Address</h4>
+                <hr class="my-4">
+                <div class="small text-black-50">Jalan Terusan Ryacudu, Desa Way Hui, Kecamatan Jati Agung, Lampung Selatan 35365</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fas fa-envelope text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Email</h4>
+                <hr class="my-4">
+                <div class="small text-black-50">
+                  <a href="#">admin@visitlampung.com</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Phone</h4>
+                <hr class="my-4">
+                <div class="small text-black-50">+62 (21) 902-8832</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="social d-flex justify-content-center">
+          <a href="<?php echo 'https://www.twitter.com';?>" class="mx-2">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="<?php echo 'https://www.facebook.com';?>" class="mx-2">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="<?php echo 'https://github.com/';?>" class="mx-2">
+            <i class="fab fa-github"></i>
+          </a>
+        </div>
+
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="bg-black small text-center text-white-50">
+      <div class="container">
+        Copyright &copy; Informatic Engineering - Institut Teknologi Sumatera 2018
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url('assets/jquery/jquery.min.js') ?>"></script>
+    <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo base_url('assets/jquery-easing/jquery.easing.min.js') ?>"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="<?php echo base_url('js/grayscale.min.js') ?>"></script>
+
+  </body>
+
+</html>
