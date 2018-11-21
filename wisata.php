cc39121ccd91622a6dfef927ef1634195ef3e0ed
artwin22 <muhamad.14116050@student.itera.ac.id>
2018-11-21 20:10:52 +0700

    Create wisata.php

---
+++ wisata.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>visitlampung.id - Portal Wisata Provinsi Lampung</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="<?php echo base_url('assets/fontawesome-free/css/all.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('https://fonts.googleapis.com/css?family=Varela+Round') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i') ?>" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url('css/grayscale.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('css/portofolio.css') ?>" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="<?php echo base_url().'index.php/web/overview/' ?>">visitlampung.id</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="<?php echo base_url().'index.php/web/overview/main' ?>">BACK TO HOME</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Destinasi Wisata -->
+    <section id="destinasiwisata" class="projects-section bg-dark">
+      <h1 style="text-align:center; color:white;">Destinasi Wisata</h1><br><br>
+      <div class="container">
+
+        <!-- Featured Project Row -->
+        <div class="row align-items-center no-gutters mb-4 mb-lg-5">
+          <div class="col-xl-8 col-lg-7">
+            <img class="img-fluid mb-3 mb-lg-0"src="<?php echo base_url('img/siger.jpg') ?>" alt="">
+          </div>
+          <div class="col-xl-4 col-lg-5">
+            <div class="featured-text text-center text-lg-left">
+              <h4 style="color:white">Menara Siger</h4>
+              <p class="text-white-50 mb-0">Menara Siger adalah menara yang juga menjadi titik nol Sumatra di Jalan Raya Bumi Sari Natar Gang Bima Ruko Orange.</p>
+              <br><p class="mb-0 text-white-50"><b>Alamat:</b>Jalan Lintas Sumatra, Bakauheni, Kabupaten Lampung Selatan, Lampung 35592</p>
+            </div>
+          </div>
+        </div>
+
+
+        <!-- Project One Row -->
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="<?php echo base_url('img/muncaktirtayasa.jpg') ?>"  alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white">Muncak Tirtayasa</h4>
+                  <p class="mb-0 text-white-50">Teropong laut Muncak Tirtayasa adalah spot wisata pemandangan langsung menghadap ke Teluk Lampung. </p>
+                  <br><p class="mb-0 text-white-50"><b>Alamat:</b> Gunung Muncak, Sukajaya Lempasing, Padang Cermin, Sukajaya Lempasing, Padang Cermin,Kabupaten Pesawaran, Lampung 35236</p>
+                  <hr class="d-none d-lg-block mb-0 ml-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Project Two Row -->
+        <div class="row justify-content-center no-gutters">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="<?php echo base_url('img/waykambas.jpg') ?>"  alt="">
+          </div>
+          <div class="col-lg-6 order-lg-first">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-right">
+                  <h4 class="text-white">Taman Nasional Way Kambas</h4>
+                  <p class="mb-0 text-white-50">Taman Nasional Way Kambas adalah taman nasional perlindungan gajah yang terletak di daerah Lampung tepatnya di Kecamatan Labuhan Ratu, Lampung Timur, Indonesia. </p>
+                  <br><p class="mb-0 text-white-50"><b>Alamat: </b>Kabupaten Lampung Timur, Lampung</p>
+                  <hr class="d-none d-lg-block mb-0 mr-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+<!--Start ROW-->
+      <div class="container">
+
+        <!-- Project Three Row -->
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="<?php echo base_url('img/kiluan.jpg') ?>"  alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white">Teluk Kiluan</h4>
+                  <p class="mb-0 text-white-50">Teluk Kiluan adalah objek wisata pantai yang terletak di Bumi Sari Natar, Kabupaten Tanggamus Provinsi Lampung dengan potensi wisata bahari, dan terkenal dengan banyaknya lumba-lumba, ikan paus disekitaran Teluk Kiluan. Teluk Kiluan juga terkenal dengan keindahan alam dan surga bagi para pemancing handal.</p>
+                  <hr class="d-none d-lg-block mb-0 ml-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Project Four Row -->
+        <div class="row justify-content-center no-gutters">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="<?php echo base_url('img/pahawang.jpg') ?>"  alt="">
+          </div>
+          <div class="col-lg-6 order-lg-first">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-right">
+                  <h4 class="text-white">Pulau Pahawang</h4>
+                  <p class="mb-0 text-white-50">Pulau Pahawang ini terletak lepas Teluk Punduh Letak Tujuan Di Jalan Raya Bumi Sari Natar Gang Bima Ruko Orange memiliki sejuta pesona yang tidak kalah menarik dengan objek lainnya.</p>
+                  <hr class="d-none d-lg-block mb-0 mr-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+<!--END ROW-->
+
+    </section>
+
+    <!-- Footer -->
+    <footer class="bg-black small text-center text-white-50">
+      <div class="container">
+        Copyright &copy; Informatic Engineering - Institut Teknologi Sumatera 2018
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url('assets/jquery/jquery.min.js') ?>"></script>
+    <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo base_url('assets/jquery-easing/jquery.easing.min.js') ?>"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="<?php echo base_url('js/grayscale.min.js') ?>"></script>
+
+  </body>
+
+</html>
