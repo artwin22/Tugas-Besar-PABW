177b44cf60c3cf4361e2930c8fb12ee7f4aebe67
artwin22 <muhamad.14116050@student.itera.ac.id>
2018-11-21 19:55:54 +0700

    Create main.php

---
+++ application/views/web/main.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>visitlampung.com - Portal Wisata Provinsi Lampung</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="<?php echo base_url('assets/fontawesome-free/css/all.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('https://fonts.googleapis.com/css?family=Varela+Round') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i') ?>" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url('css/grayscale.min.css') ?>" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">visitlampung.com</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="<?php echo base_url().'index.php/web/overview/main' ?>">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#destinasiwisata">DESTINASI WISATA</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#festival">FESTIVAL</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#aboutus">ABOUT US</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead">
+      <div class="container d-flex h-100 align-items-center">
+        <div class="mx-auto text-center">
+          <img src="<?php echo base_url('img/visitlampung.png') ?>" alt="Visit Lampung" style="width:768px;">
+          <h2 class="text-white-50 mx-auto mt-2 mb-5">Our Pride, Your Destination</h2>
+          <a href="#home" class="btn btn-primary js-scroll-trigger">Tentang Lampung</a>
+        </div>
+      </div>
+    </header>
+
+    <!-- About Section -->
+    <section id="home" class="about-section text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <img src="<?php echo base_url('img/lampung.png') ?>" alt="Lampung" style="width:30%;">
+            <h2 class="text-white mb-4">Lampung</h2>
+            <p class="text-white-50">Provinsi Lampung dikenal juga dengan julukan “Sang Bumi Ruwa Jurai” yang berarti satu bumi yang didiami oleh dua macam masyarakat (suku/etnis), yaitu masyarakat Pepadun dan Saibatin. Masyarakat pertama mendiami daratan dan pedalaman Lampung, seperti daerah Tulang Bawang, Abung, Sungkai, Way Kanan, dan Pubian, sedangkan masyarakat kedua mendiami daerah pesisir pantai, seperti Labuhan Maringgai, Pesisir Krui, Pesisir Semangka (Wonosobo dan Kota Agung), Balalau, dan Pesisir Rajabasa.</p>
+          </div>
+          </div>
+        </div>
+    </section>
+
+    <!-- Destinasi Wisata -->
+    <section id="destinasiwisata" class="projects-section bg-light">
+      <h1 style="text-align:center;">Destinasi Wisata</h1><br><br>
+      <div class="container">
+
+        <!-- Featured Project Row -->
+        <div class="row align-items-center no-gutters mb-4 mb-lg-5">
+          <div class="col-xl-8 col-lg-7">
+            <img class="img-fluid mb-3 mb-lg-0"src="<?php echo base_url('img/siger.jpg') ?>" alt="">
+          </div>
+          <div class="col-xl-4 col-lg-5">
+            <div class="featured-text text-center text-lg-left">
+              <h4>Menara Siger</h4>
+              <p class="text-black-50 mb-0">Menara Siger adalah menara yang juga menjadi titik nol Sumatra di Jalan Raya Bumi Sari Natar Gang Bima Ruko Orange.</p>
+              <br><p class="mb-0 text-black-50"><b>Alamat:</b>Jalan Lintas Sumatra, Bakauheni, Kabupaten Lampung Selatan, Lampung 35592</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- Project One Row -->
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="<?php echo base_url('img/muncaktirtayasa.jpg') ?>"  alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white">Muncak Tirtayasa</h4>
+                  <p class="mb-0 text-white-50">Teropong laut Muncak Tirtayasa adalah spot wisata pemandangan langsung menghadap ke Teluk Lampung. </p>
+                  <br><p class="mb-0 text-white-50"><b>Alamat:</b> Gunung Muncak, Sukajaya Lempasing, Padang Cermin, Sukajaya Lempasing, Padang Cermin,Kabupaten Pesawaran, Lampung 35236</p>
+                  <hr class="d-none d-lg-block mb-0 ml-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Project Two Row -->
+        <div class="row justify-content-center no-gutters">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="<?php echo base_url('img/waykambas.jpg') ?>"  alt="">
+          </div>
+          <div class="col-lg-6 order-lg-first">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-right">
+                  <h4 class="text-white">Taman Nasional Way Kambas</h4>
+                  <p class="mb-0 text-white-50">Taman Nasional Way Kambas adalah taman nasional perlindungan gajah yang terletak di daerah Lampung tepatnya di Kecamatan Labuhan Ratu, Lampung Timur, Indonesia. </p>
+                  <br><p class="mb-0 text-white-50"><b>Alamat: </b>Kabupaten Lampung Timur, Lampung</p>
+                  <hr class="d-none d-lg-block mb-0 mr-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br><br><br>
+        <p style="font-size:20px">Wisata Selengkapnya ... [<a href="<?php echo base_url().'index.php/web/overview/wisata' ?>"style="text-decoration ; underline">Read More</a>]</p>
+        <hr style="border-width: 4px;">
+      </div>
+    </section>
+
+    <!-- Festival -->
+    <section id="festival" class="projects-section bg-light">
+      <h1 style="text-align:center;">Festival</h1><br><br>
+      <div class="container">
+
+        <!-- Featured Project Row -->
+        <div class="row align-items-center no-gutters mb-4 mb-lg-5">
+          <div class="col-xl-8 col-lg-7">
+            <img class="img-fluid mb-3 mb-lg-0"src="<?php echo base_url('img/krakataufest.jpg') ?>" alt="">
+          </div>
+          <div class="col-xl-4 col-lg-5">
+            <div class="featured-text text-center text-lg-left">
+              <h4>Festival Krakatau</h4>
+              <p class="text-black-50 mb-0">Festival Krakatau merupakan salah satu perhelatan kebudayaan unggulan dari Provinsi Lampung. Festival yang rutin diadakan setiap tahun ini merupakan parade kebudayaan yang mengangkat kekayaan budaya dan tradisi yang dimiliki Lampung. Selain itu, perhelatan ini juga menjadi ajang promosi potensi pariwisata yang dimiliki setiap kabupaten dan kota yang ada di Lampung.</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- Project One Row -->
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="<?php echo base_url('img/sekurafestival.jpg') ?>"  alt="">
+          </div>
+          <div class="col-lg-6">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 class="text-white">Pesta Sekura</h4>
+                  <p class="mb-0 text-white-50">Pesta sekura merupakan perhelatan rutin yang diadakan oleh masyarakat Kabupaten Lampung Barat. Pesta rakyat ini selalu diadakan ketika menyambut Hari Raya Idul Fitri. Dalam acara ini, peserta acara diwajibkan mengenakan topeng dengan berbagai karakter dan ekspresi. Pesta sekura merupakan wujud ungkapan rasa syukur dan suka cita menyambut hari yang suci.</p>
+                  <hr class="d-none d-lg-block mb-0 ml-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Project Two Row -->
+        <div class="row justify-content-center no-gutters">
+          <div class="col-lg-6">
+            <img class="img-fluid" src="<?php echo base_url('img/megoupak.png') ?>"  alt="">
+          </div>
+          <div class="col-lg-6 order-lg-first">
+            <div class="bg-black text-center h-100 project">
+              <div class="d-flex h-100">
+                <div class="project-text w-100 my-auto text-center text-lg-right">
+                  <h4 class="text-white">Festival Megou Pak</h4>
+                  <p class="mb-0 text-white-50">Kegiatan Festival Megou Pak ini salah satu bentuk kemeriahan Hari Ulang Tahun (HUT)  Tulangbawang, kegiatan ini juga diharapkan dapat menjadi suatu ajang promosi objek wisata ke luar daerah Tulangbawang serta hingga pelosok luar daerah Provinsi Lampung.</p>
+                  <hr class="d-none d-lg-block mb-0 mr-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- About dan Komentar -->
+    <section id="aboutus" class="signup-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 col-lg-8 mx-auto text-center">
+
+            <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
+            <h2 class="text-white mb-5">Rekomendasikan Tempat Wisata Baru!</h2>
+            <input type="text" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail" placeholder="Rekomendasikan Tempat Wisata...">
+            <button type="submit" class="btn btn-primary mx-auto">Recommended</button>
+
+        </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section class="contact-section bg-black">
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Address</h4>
+                <hr class="my-4">
+                <div class="small text-black-50">Jalan Terusan Ryacudu, Desa Way Hui, Kecamatan Jati Agung, Lampung Selatan 35365</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fas fa-envelope text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Email</h4>
+                <hr class="my-4">
+                <div class="small text-black-50">
+                  <a href="#">admin@visitlampung.com</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card py-4 h-100">
+              <div class="card-body text-center">
+                <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                <h4 class="text-uppercase m-0">Phone</h4>
+                <hr class="my-4">
+                <div class="small text-black-50">+62 (21) 902-8832</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="social d-flex justify-content-center">
+          <a href="<?php echo 'https://www.twitter.com';?>" class="mx-2">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="<?php echo 'https://www.facebook.com';?>" class="mx-2">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="<?php echo 'https://github.com/';?>" class="mx-2">
+            <i class="fab fa-github"></i>
+          </a>
+        </div>
+
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="bg-black small text-center text-white-50">
+      <div class="container">
+        Copyright &copy; Informatic Engineering - Institut Teknologi Sumatera 2018
+        <br><hr style="background-color: #515151; width:40%">
+        Developer Team <a href="<?php echo base_url().'index.php/web/overview/developer'?>">Click Here!</a>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url('assets/jquery/jquery.min.js') ?>"></script>
+    <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo base_url('assets/jquery-easing/jquery.easing.min.js') ?>"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="<?php echo base_url('js/grayscale.min.js') ?>"></script>
+
+  </body>
+
+</html>
